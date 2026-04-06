566d30d3507599a59caf878121782e925d29ffd7
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-04-06 19:00:50 +0700

    tampilkan

--- arrayStruck.cpp
+++ arrayStruck.cpp
@@ -32,4 +32,14 @@ int main() {
     cout << endl;
 
     }
+
+    for (int i = 0; i <= 2; i++)
+    
+    {
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs[i].nama << endl;
+    cout << "Desa : " << mhs[i].alamat.desa << endl;
+    cout << "Kota : " << mhs[i].alamat.kota << endl;
+    cout << "Umur : " << mhs[i].umur << endl;
+    }
 }
